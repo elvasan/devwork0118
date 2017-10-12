86d6748a5f9feaa90b0768b5d632ecb528e88c31
Peter Stratton <pstratton@jornaya.com>
2017-10-12 09:22:57 -0400

    add rdl_to_udl/campaigns transformation
    update gitignore

--- gluejobs/rdl_to_udl/campaigns.py
+++ gluejobs/rdl_to_udl/campaigns.py
@@ -1,10 +1,10 @@
 import sys
 
-from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from pyspark.sql.types import StringType, StructField, StructType
-from pyspark.sql.functions import explode, col, from_json
+from pyspark.sql.types import StringType, StructField, StructType, IntegerType, DecimalType
+from pyspark.sql.functions import col, from_json
 
+from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.dynamicframe import DynamicFrame
@@ -23,7 +23,8 @@ db_name = 'rdl'
 tbl_name = 'campaigns'
 
 # output directories
-output_dir = "s3://jornaya-dev-us-east-1-etl-code/glue/jobs/output/{}".format(args['JOB_NAME'])
+# TODO: pass these file paths in as args instead of hardcoding them
+output_dir = "s3://jornaya-dev-us-east-1-udl/{}".format(tbl_name)
 staging_dir = "s3://jornaya-dev-us-east-1-etl-code/glue/jobs/staging/{}".format(args['JOB_NAME'])
 temp_dir = "s3://jornaya-dev-us-east-1-etl-code/glue/jobs/tmp/{}".format(args['JOB_NAME'])
 
@@ -31,39 +32,51 @@ temp_dir = "s3://jornaya-dev-us-east-1-etl-code/glue/jobs/tmp/{}".format(args['J
 campaigns = glueContext.create_dynamic_frame.from_catalog(database=db_name,
                                                           table_name=tbl_name,
                                                           transformation_ctx='campaigns')
+
 df = campaigns.toDF()
 
-keys = (df
-        .select(explode("item"))
-        .select('key')
-        .distinct()
-        .rdd.flatMap(lambda x: x)
-        .collect())
+keys = [
+    'account_code',
+    'audience_id_tags',
+    'campaign_javascript_version',
+    'created',
+    'created_by',
+    'description',
+    'forensiq_default',
+    'hash_urls',
+    'key',
+    'log_level',
+    'log_limit',
+    'log_targets',
+    'modified',
+    'modified_by',
+    'name',
+    'threatmetrix_default'
+]
 
 exprs = [col("item").getItem(k).alias(k) for k in keys]
-
 df = df.select(*exprs)
 
 n_schema = StructType([StructField("n", StringType())])
 s_schema = StructType([StructField("s", StringType())])
 sS_schema = StructType([StructField("sS", StringType())])
 
-df = df.select(from_json(df.account_code, s_schema).getItem('s').alias('account_code'),
-               from_json(df.audience_id_tags, sS_schema).getItem('sS').alias('audience_id_tags'),
-               from_json(df.campaign_javascript_version, s_schema).getItem('s').alias('campaign_javascript_version'),
-               from_json(df.created, n_schema).getItem('n').alias('created'),
-               from_json(df.created_by, n_schema).getItem('n').alias('created_by'),
-               from_json(df.description, s_schema).getItem('s').alias('description'),
-               from_json(df.forensiq_default, n_schema).getItem('n').alias('forensiq_default'),
-               from_json(df.hash_urls, n_schema).getItem('n').alias('hash_urls'),
-               from_json(df.key, s_schema).getItem('s').alias('key'),
-               from_json(df.log_level, n_schema).getItem('n').alias('log_level'),
-               from_json(df.log_limit, n_schema).getItem('n').alias('log_limit'),
-               from_json(df.log_targets, n_schema).getItem('n').alias('log_targets'),
-               from_json(df.modified, n_schema).getItem('n').alias('modified'),
-               from_json(df.modified_by, n_schema).getItem('n').alias('modified_by'),
-               from_json(df.name, s_schema).getItem('s').alias('name'),
-               from_json(df.threatmetrix_default, n_schema).getItem('n').alias('threatmetrix_default'))
+df = df.select(from_json(df.account_code, s_schema).getItem('s').alias('account_code').cast(IntegerType()),
+               from_json(df.audience_id_tags, sS_schema).getItem('sS').alias('audience_id_tags').cast(StringType()),
+               from_json(df.campaign_javascript_version, s_schema).getItem('s').alias('campaign_javascript_version').cast(StringType()),
+               from_json(df.created, n_schema).getItem('n').alias('created').cast(DecimalType(14, 4)),
+               from_json(df.created_by, n_schema).getItem('n').alias('created_by').cast(IntegerType()),
+               from_json(df.description, s_schema).getItem('s').alias('description').cast(StringType()),
+               from_json(df.forensiq_default, n_schema).getItem('n').alias('forensiq_default').cast(IntegerType()),
+               from_json(df.hash_urls, n_schema).getItem('n').alias('hash_urls').cast(IntegerType()),
+               from_json(df.key, s_schema).getItem('s').alias('key').cast(StringType()),
+               from_json(df.log_level, n_schema).getItem('n').alias('log_level').cast(IntegerType()),
+               from_json(df.log_limit, n_schema).getItem('n').alias('log_limit').cast(IntegerType()),
+               from_json(df.log_targets, n_schema).getItem('n').alias('log_targets').cast(IntegerType()),
+               from_json(df.modified, n_schema).getItem('n').alias('modified').cast(DecimalType(14, 4)),
+               from_json(df.modified_by, n_schema).getItem('n').alias('modified_by').cast(IntegerType()),
+               from_json(df.name, s_schema).getItem('s').alias('name').cast(StringType()),
+               from_json(df.threatmetrix_default, n_schema).getItem('n').alias('threatmetrix_default').cast(IntegerType()))
 
 dyf = DynamicFrame.fromDF(df, glueContext, "dyf")
 
