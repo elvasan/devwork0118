ef9e86246fd9416f4eff1789ad0b157a04ea353e
Dan Kelly <dkelly@jornaya.com>
2017-12-11 13:58:12 -0500

    Remove Opt In Timestamp from Account and Campaign Event
    This commit removes opt in timestamp from being considered in the account and campaign event tables. There was also some additional validation added to the campaign, account and view.

--- gluejobs/rdl_to_prj/campaign_opt_in_event.py
+++ gluejobs/rdl_to_prj/campaign_opt_in_event.py
@@ -4,7 +4,7 @@ from awsglue.context import GlueContext  # pylint: disable=import-error
 from awsglue.job import Job  # pylint: disable=import-error
 from awsglue.utils import getResolvedOptions  # pylint: disable=import-error
 from pyspark.context import SparkContext
-from pyspark.sql.functions import col, to_utc_timestamp, when
+from pyspark.sql.functions import col, when, lower
 from pyspark.sql.types import ShortType
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -15,27 +15,36 @@ glueContext = GlueContext(sc)
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-TABLE_NAME = 'campaign_opt_in_event'
+CAMPAIGN_TABLE_NAME = 'campaign_opt_in_event'
+APPLICATION = 'application'
+COL_CAMPAIGN_KEY = 'campaign_key'
+COL_STATE = 'state'
+COL_APPLICATION_KEY = 'application_key'
+COL_OPT_IN_IND = 'opt_in_ind'
 
 # Define the output directory
-output_dir = "s3://jornaya-dev-us-east-1-prj/publisher_permissions/setup/{}".format(TABLE_NAME)
+output_dir = "s3://jornaya-dev-us-east-1-prj/publisher_permissions/setup/{}".format(CAMPAIGN_TABLE_NAME)
 
 campaign_opt_in_event_df = glueContext.create_dynamic_frame \
-    .from_catalog(database='rdl', table_name=TABLE_NAME) \
+    .from_catalog(database='rdl', table_name=CAMPAIGN_TABLE_NAME) \
     .toDF() \
-    .dropDuplicates()
+    .select(COL_CAMPAIGN_KEY, COL_STATE, APPLICATION) \
+    .dropna(subset=[COL_CAMPAIGN_KEY])
 
 # Grab application table so we can join application to the campaigns DataFrame and get app key
 application_df = glueContext.create_dynamic_frame \
-    .from_catalog(database='prj', table_name='application') \
+    .from_catalog(database='prj', table_name=APPLICATION) \
     .toDF()
 
 campaign_opt_in_event = campaign_opt_in_event_df \
-    .join(application_df, campaign_opt_in_event_df.application == application_df.application_nm) \
-    .select('campaign_key', 'state', 'application_key', 'date') \
-    .withColumn('opt_in_ts', to_utc_timestamp(campaign_opt_in_event_df.date, 'UTC')) \
-    .withColumn('opt_in_ind', (when(col('state') == 'In', 1).otherwise(0)).cast(ShortType())) \
-    .drop('state', 'date')
+    .join(application_df, lower(campaign_opt_in_event_df.application) == lower(application_df.application_nm)) \
+    .select(COL_CAMPAIGN_KEY, COL_STATE, COL_APPLICATION_KEY) \
+    .withColumn(COL_OPT_IN_IND, (when(lower(col(COL_STATE)) == 'in', 1)
+                                 .otherwise(when(lower(col(COL_STATE)) == 'out', 0)
+                                            .otherwise(None))).cast(ShortType())) \
+    .dropna(subset=[COL_OPT_IN_IND]) \
+    .drop(COL_STATE) \
+    .dropDuplicates([COL_CAMPAIGN_KEY, COL_APPLICATION_KEY])
 
 campaign_opt_in_event.write.parquet(output_dir, mode='overwrite')
 
