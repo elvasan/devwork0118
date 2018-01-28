7360ca0f2156de0ada1fc149e1c25a455f23db27
Dan Kelly <dkelly@jornaya.com>
2018-01-28 15:49:10 -0500

    Ability to deploy dev jobs
    This commit adds the ability to include a user flag in the deploy script which will create glue jobs with a username prefix appended to the job name.

--- glue_deployer/app.py
+++ glue_deployer/app.py
@@ -1,5 +1,5 @@
-import logging
 import argparse
+import logging
 
 from glue_deployer.deployer_utils import update_or_create_job, destroy_job, \
     update_or_create_all_jobs_for_stage, destroy_all_jobs_for_stage
@@ -24,6 +24,7 @@ def create_parser():
     parser_create_jobs.set_defaults(which='job-create')
     parser_create_jobs.add_argument('-e', '--environment', help='Environment name to use in the S3 buckets')
     parser_create_jobs.add_argument('-j', '--jobfile', help='Local path to the glue job pyspark file')
+    parser_create_jobs.add_argument('-u', '--user', help='User name used for folder creation under etl-code')
     parser_create_jobs.add_argument('-a', '--all', help='Stage name to create all jobs for.')
     parser_create_jobs.add_argument('-v', '--verbose', action='store_true', help='Turns debug logging on, very chatty.')
 
@@ -31,6 +32,7 @@ def create_parser():
     parser_destroy_jobs = subparsers.add_parser('job-destroy', help='Destroys an existing AWS Glue Job.')
     parser_create_jobs.set_defaults(which='job-destroy')
     parser_destroy_jobs.add_argument('-e', '--environment', help='Environment name to use in the S3 buckets')
+    parser_destroy_jobs.add_argument('-u', '--user', help='User for which you want to delete jobs')
     parser_destroy_jobs.add_argument('-j', '--jobfile', help='Local path to the glue job pyspark file')
     parser_destroy_jobs.add_argument('-a', '--all', help='Stage name to destroy all jobs for.')
     parser_destroy_jobs.add_argument('-v', '--verbose', action='store_true', help='Turn debug logging on, very chatty.')
@@ -46,23 +48,30 @@ def main():
     if args.subcommand == 'job-create':
         if args.verbose:
             LOGGER.setLevel(logging.DEBUG)
+        if not args.user:
+            args.user = 'glue'
         if args.all:
             update_or_create_all_jobs_for_stage(args.all,
-                                                args.environment)
+                                                args.environment,
+                                                args.user)
         else:
             update_or_create_job(jobfile=args.jobfile,
-                                 env=args.environment)
+                                 env=args.environment,
+                                 user=args.user)
     elif args.subcommand == 'job-destroy':
         if args.verbose:
             LOGGER.setLevel(logging.DEBUG)
         if args.all:
             destroy_all_jobs_for_stage(args.all,
-                                       args.environment)
+                                       args.environment,
+                                       args.user)
         else:
             destroy_job(jobfile=args.jobfile,
-                        env=args.environment)
+                        env=args.environment,
+                        user=args.user)
     else:
         parser.print_help()
+
 
 if __name__ == "__main__":
     main()
--- glue_deployer/test_deployer_utils.py
+++ glue_deployer/test_deployer_utils.py
@@ -11,11 +11,11 @@ class TestDeployerUtilFunctions(unittest.TestCase):
         self.assertEqual('foo', u._filename_from_filepath('/baz/bar/foo.txt'))
 
     def test_jobname_from_file(self):
-        self.assertEqual('bar/foo', u._jobname_from_file('bar/foo.txt'))
-        self.assertEqual('baz/bar/foo', u._jobname_from_file('baz/bar/foo.txt'))
+        self.assertEqual('dkelly_bar/foo', u._jobname_from_file('dkelly', 'bar/foo.txt'))
+        self.assertEqual('dkelly_baz/bar/foo', u._jobname_from_file('dkelly', 'baz/bar/foo.txt'))
 
     def test_package_path_from_jobfile(self):
-        self.assertEqual('glue/jobs/bar/baz_libs.zip', u._package_path_from_jobfile('bar/baz.txt', 'baz_libs.zip'))
+        self.assertEqual('dkelly/glue/jobs/dkelly_bar/baz_libs.zip', u._package_path_from_jobfile('bar/baz.txt', 'baz_libs.zip', 'dkelly'))
 
     def test_get_job_bucket(self):
         self.assertEqual('jornaya-dev-us-east-1-etl-code', u._get_job_bucket('dev'))
@@ -24,7 +24,7 @@ class TestDeployerUtilFunctions(unittest.TestCase):
 
     def test_build_job_dict(self):
         expected = {
-            'Name': 'bar/foo',
+            'Name': 'dkelly_bar/foo',
             'Role': 'Glue_DefaultRole',
             'ExecutionProperty': {
                 'MaxConcurrentRuns': 1
@@ -34,14 +34,15 @@ class TestDeployerUtilFunctions(unittest.TestCase):
                 'ScriptLocation': '/foo/bar/foo.txt'
             },
             'DefaultArguments': {
-                '--TempDir': 's3://jornaya-dev-us-east-1-etl-code/glue/jobs/tmp/bar/foo/',
+                '--TempDir': 's3://jornaya-dev-us-east-1-etl-code/dkelly/glue/jobs/tmp/dkelly_bar/foo/',
                 '--job-bookmark-option': 'job-bookmark-disable',
-                '--extra-py-files': 's3://jornaya-dev-us-east-1-etl-code/glue/jobs/bar/foo_libs.zip'
+                '--extra-py-files': 's3://jornaya-dev-us-east-1-etl-code/dkelly/glue/jobs/dkelly_bar/foo_libs.zip'
             },
             'MaxRetries': 0,
             'AllocatedCapacity': 2
         }
-        self.assertEqual(expected, u._build_job_dict('bar/foo.txt', '/foo/bar/foo.txt', 'dev'))
+        self.assertEqual(expected, u._build_job_dict('bar/foo.txt', '/foo/bar/foo.txt', 'dev', 'dkelly'))
+
 
 if __name__ == '__main__':
     unittest.main()
